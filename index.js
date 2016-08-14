cc02065673bfd700600ab93cfa4e05b7f467eca4
gcid <gcid12@gmail.com>
2016-08-14 14:12:00 -0400

    nginx config

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-var express = require('express')
-  , http = require('http')
-  , config = require('./configuration');
+// set up nodejs web server to run behind nginx proxy
 
-var app = express();
-var server = app.listen(config.port);
-console.log("Express server listening on port "+ config.port);
+    var http = require('http')
+    , express = require('express')
+    ;
+    var app = express();
 
-app.get('/',index);
+    /* enable handy to run behind a proxy e.g. nginx.
+     * comment this out if using the node web server
+     * to serve requests directly
+     */
+    app.enable('trust proxy');
 
-function index(req,res){
-   res.send('Hello ' + config.say);
-}
+    http.createServer(app).listen(3000, 127.0.0.1, function(){
+      // process http requests here
+      res.send("Wasssup!");
+    });
+
+
+
+    //PREVIOUS
+
+// var express = require('express')
+//   , http = require('http')
+//   , config = require('./configuration');
+
+// var app = express();
+// var server = app.listen(config.port);
+// console.log("Express server listening on port "+ config.port);
+
+// app.get('/',index);
+
+// function index(req,res){
+//    res.send('Hello ' + config.say);
+// }
+
+
