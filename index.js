d09235da39f5cd384d5b2a5ce2f36a8a7ca206bc
gcid <gcid12@gmail.com>
2016-08-14 18:01:39 -0400

    hello af

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
-// set up nodejs web server to run behind nginx proxy
 
-    var http = require('http')
-    , express = require('express')
-    ;
-    var app = express();
+var express = require('express')
+  , http = require('http')
+  , config = require('./configuration')
+  , path = require("path");
 
-    /* enable handy to run behind a proxy e.g. nginx.
-     * comment this out if using the node web server
-     * to serve requests directly
-     */
-    app.enable('trust proxy');
+var app = express();
+var server = app.listen(config.port);
+console.log("Express server listening on port "+ config.port);
 
-    http.createServer(app).listen(3000, 127.0.0.1, function(){
-      // process http requests here
-      res.send("Wasssup!");
-    });
+app.get('/',index);
 
-
-
-    //PREVIOUS
-
-// var express = require('express')
-//   , http = require('http')
-//   , config = require('./configuration');
-
-// var app = express();
-// var server = app.listen(config.port);
-// console.log("Express server listening on port "+ config.port);
-
-// app.get('/',index);
-
-// function index(req,res){
-//    res.send('Hello ' + config.say);
-// }
+function index(req,res){
+   //res.send('HHello ' + config.say);
+   res.sendFile(path.join(__dirname+'/index.html'));
+}
 
 
